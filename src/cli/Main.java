4f942da13bb5ecd48814fdc9332afbe0568b644b
leon heschl <heschl.leon@gmail.com>
2026-03-15 18:58:38 +0100

    Setup initial chess-bot project structure

---
+++ src/cli/Main.java
@@ -0,0 +1,17 @@
+package cli;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Bot bot = new Bot(board)
+        Board board = new Board();
+
+        while() {
+            board.update(Cli.getUserMove)
+            Move move = Bot.getMove()
+            print(move)
+        }
+
+         */
+    }
+}
