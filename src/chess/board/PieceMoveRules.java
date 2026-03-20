b452c8d16dbffbcea3e27a2ddc0e0a132adc3457
The.Only.Sensenmann <jannik.sensenberger@gmail.com>
2026-03-20 17:51:54 +0100

    finish PieceMoveRules - except Pawn, still in y/x form, need to change that

--- src/chess/board/PieceMoveRules.java
+++ src/chess/board/PieceMoveRules.java
@@ -2,8 +2,13 @@ package chess.board;
 
 
 public enum PieceMoveRules {
-    ROOK({{-1, -1}, {-1, 0}, {-1, 1}, { 0, -1},{ 0, 1},{ 1, -1}, { 1, 0}, { 1, 1}}, false, true),
-    BISHOP(), QUEEN(), KNIGHT(), PAWN(), KING();
+    QUEEN(new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, { 0, -1},{ 0, 1},{ 1, -1}, { 1, 0}, { 1, 1}}, false, true),
+    BISHOP(new int[][]{{1, 1}, {1, -1}, {-1, 1}, {-1, -1}}, false, true),
+    ROOK(new int[][]{{-1, 0},{0, -1},{0, 1},{1, 0}}, false, true),
+    KNIGHT(new int[][]{{-2, 1}, {-2, -1}, {-1, 2}, {-1, -2}, {1, 2}, {1, -2}, {2, 1}, {2, -1}}, true, false),
+    PAWN(),
+    KING(new int[][]{{-1, -1}, {-1, 0}, {-1, 1},{ 0, -1},{ 0, 1},{ 1, -1}, { 1, 0}, { 1, 1}}, false, false);
+
 
     private int[][] directions;
     private boolean canJump;
@@ -14,5 +19,18 @@ public enum PieceMoveRules {
         this.directions = directions;
         this.canJump = canJump;
         this.canMoveInfinitely = canMoveInfinitely;
+    }
+
+
+    public int[][] getDirections() {
+        return directions;
+    }
+
+    public boolean canJump() {
+        return canJump;
+    }
+
+    public boolean canMoveInfinitely() {
+        return canMoveInfinitely;
     }
 }
