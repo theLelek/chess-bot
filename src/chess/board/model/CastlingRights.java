25fed558e110543eeacd40192f8321b5939b5e02
leon heschl <heschl.leon@gmail.com>
2026-03-31 20:12:15 +0200

    added constructor for copying CastlingRights instance

--- src/chess/board/model/CastlingRights.java
+++ src/chess/board/model/CastlingRights.java
@@ -2,7 +2,20 @@ package chess.board.model;
 
 import chess.Color;
 
-public record CastlingRights(boolean canCastleKingSide, boolean canCastleQueenSide) {
+public class CastlingRights {
+
+    private final boolean canCastleKingSide;
+    private final boolean canCastleQueenSide;
+
+    public CastlingRights(boolean canCastleKingSide, boolean canCastleQueenSide) {
+        this.canCastleKingSide = canCastleKingSide;
+        this.canCastleQueenSide = canCastleQueenSide;
+    }
+
+    public CastlingRights(CastlingRights castlingRights) {
+        this.canCastleKingSide = castlingRights.canCastleKingSide;
+        this.canCastleQueenSide = castlingRights.canCastleQueenSide;
+    }
 
     public static CastlingRights fromFen(String fen, Color color) {
         boolean kingSide;
