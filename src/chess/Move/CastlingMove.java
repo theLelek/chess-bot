3257410272c7534bbaa12f2b9722f589bfb61af6
Leon Heschl <heschl.leon@gmail.com>
2026-05-07 14:16:56 +0200

    created classes for every move type

---
+++ src/chess/Move/CastlingMove.java
@@ -0,0 +1,9 @@
+package chess.Move;
+
+import chess.BoardPosition;
+
+public class CastlingMove extends Move {
+    public CastlingMove(BoardPosition from, BoardPosition to) {
+        super(from, to);
+    }
+}
---
+++ src/chess/Move/EnPassentMove.java
@@ -0,0 +1,9 @@
+package chess.Move;
+
+import chess.BoardPosition;
+
+public class EnPassentMove extends Move {
+    public EnPassentMove(BoardPosition from, BoardPosition to) {
+        super(from, to);
+    }
+}
