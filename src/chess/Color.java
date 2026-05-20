2c0a9832b0ec6e6cf1f1f9246b24447b2d13bcc9
Leon Heschl <heschl.leon@gmail.com>
2026-05-20 13:26:57 +0200

    added own and opponent pieces - type: PieceBitBoard

--- src/chess/Color.java
+++ src/chess/Color.java
@@ -1,19 +1,23 @@
 package chess;
 
 import chess.Move.CastlingMove;
+import chess.board.model.PieceBitboard;
+
 
 public enum Color {
 
     WHITE(
             7, 0, -1,
             new CastlingMove(4, 7, 6, 7), // kingside
-            new CastlingMove(4, 7, 2, 7)  // queenside
+            new CastlingMove(4, 7, 2, 7),  // queenside
+            PieceBitboard.BLACK_PIECES
     ),
 
     BLACK(
             0, 7, 1,
             new CastlingMove(4, 0, 6, 0), // kingside
-            new CastlingMove(4, 0, 2, 0)  // queenside
+            new CastlingMove(4, 0, 2, 0),  // queenside
+            PieceBitboard.WHITE_PIECES
     );
 
     private final int homeRank;
@@ -21,13 +25,15 @@ public enum Color {
     private final int movingDirection;
     private final CastlingMove castlingMoveKingSide;
     private final CastlingMove castlingMoveQueenSide;
+    private final PieceBitboard opponentPieces;
 
-    Color(int homeRank, int backRank, int movingDirection, CastlingMove castlingMoveKingSide, CastlingMove castlingMoveQueenSide) {
+    Color(int homeRank, int backRank, int movingDirection, CastlingMove castlingMoveKingSide, CastlingMove castlingMoveQueenSide, PieceBitboard opponentPieces) {
         this.homeRank = homeRank;
         this.backRank = backRank;
         this.movingDirection = movingDirection;
         this.castlingMoveKingSide = castlingMoveKingSide;
         this.castlingMoveQueenSide = castlingMoveQueenSide;
+        this.opponentPieces = opponentPieces;
     }
 
     public CastlingMove getCastlingMoveKingSide() {
@@ -48,5 +54,9 @@ public enum Color {
 
     public int getMovingDirection() {
         return movingDirection;
+    }
+
+    public PieceBitboard getOpponentPieces() {
+        return opponentPieces;
     }
 }
