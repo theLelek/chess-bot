3b6ba6bd59c4101a64ce7a70f49ae9a83260defe
leon heschl <heschl.leon@gmail.com>
2026-05-27 16:43:17 +0200

    removed not needed instance variables in enum

--- src/chess/board/model/PieceBitboard.java
+++ src/chess/board/model/PieceBitboard.java
@@ -2,20 +2,6 @@ package chess.board.model;
 
 public enum PieceBitboard implements BitboardIndexProvider {
 
-    WHITE_PAWNS(0),
-    WHITE_KNIGHTS(1),
-    WHITE_BISHOPS(2),
-    WHITE_ROOKS(3),
-    WHITE_QUEEN(4),
-    WHITE_KING(5),
-
-    BLACK_PAWNS(6),
-    BLACK_KNIGHTS(7),
-    BLACK_BISHOPS(8),
-    BLACK_ROOKS(9),
-    BLACK_QUEEN(10),
-    BLACK_KING(11),
-
     WHITE_PIECES(12),
     BLACK_PIECES(13),
     ALL_PIECES(14);
@@ -24,25 +10,6 @@ public enum PieceBitboard implements BitboardIndexProvider {
 
     PieceBitboard(int bitboardIndex) {
         this.bitboardIndex = bitboardIndex;
-    }
-
-    public static PieceBitboard fromFen(char fen) {
-        return switch (fen) {
-            case 'P' -> PieceBitboard.WHITE_PAWNS;
-            case 'N' -> PieceBitboard.WHITE_KNIGHTS;
-            case 'B' -> PieceBitboard.WHITE_BISHOPS;
-            case 'R' -> PieceBitboard.WHITE_ROOKS;
-            case 'Q' -> PieceBitboard.WHITE_QUEEN;
-            case 'K' -> PieceBitboard.WHITE_KING;
-            case 'p' -> PieceBitboard.BLACK_PAWNS;
-            case 'n' -> PieceBitboard.BLACK_KNIGHTS;
-            case 'b' -> PieceBitboard.BLACK_BISHOPS;
-            case 'r' -> PieceBitboard.BLACK_ROOKS;
-            case 'q' -> PieceBitboard.BLACK_QUEEN;
-            case 'k' -> PieceBitboard.BLACK_KING;
-            default -> throw new IllegalArgumentException(
-                    "Invalid FEN piece: ' + fen");
-        };
     }
 
     public static PieceBitboard fromIndex(int index) {
