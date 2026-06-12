27bc40f5291f0ced097ae7c7f880cafc759a95ed
Leon Heschl <heschl.leon@gmail.com>
2026-06-12 12:20:23 +0200

    added basic move generator and board eval

--- src/main/java/dev/lelek/chess/move_generation/BoardEvaluation.java
+++ src/main/java/dev/lelek/chess/move_generation/BoardEvaluation.java
@@ -1,5 +1,8 @@
 package dev.lelek.chess.move_generation;
 
+import dev.lelek.chess.Color;
+import dev.lelek.chess.board.BoardPiece;
+import dev.lelek.chess.board.OccupancyBitboard;
 import dev.lelek.chess.board.model.Board;
 
 class BoardEvaluation {
@@ -11,30 +14,64 @@ class BoardEvaluation {
     private static final int QUEEN_VALUE = 900;
     private static final int KING_VALUE = 100_000;
 
-    public static int evaluate(Board board) {
+    public static int evaluate(Board board, Color color) {
         int value = 0;
-//        for (BoardPosition position : board.getPiecesIndexes()) {
-//            BoardPiece piece = board.getBoardPieces()[position.y()][position.x()];
-//            switch (piece) {
-//                case WHITE_PAWN -> value += PAWN_VALUE;
-//                case BLACK_PAWN -> value -= PAWN_VALUE;
-//
-//                case WHITE_KNIGHT -> value += KNIGHT_VALUE;
-//                case BLACK_KNIGHT -> value -= KNIGHT_VALUE;
-//
-//                case WHITE_BISHOP -> value += BISHOP_VALUE;
-//                case BLACK_BISHOP -> value -= BISHOP_VALUE;
-//
-//                case WHITE_ROOK -> value += ROOK_VALUE;
-//                case BLACK_ROOK -> value -= ROOK_VALUE;
-//
-//                case WHITE_QUEEN -> value += QUEEN_VALUE;
-//                case BLACK_QUEEN -> value -= QUEEN_VALUE;
-//
-//                case WHITE_KING -> value += KING_VALUE;
-//                case BLACK_KING -> value -= KING_VALUE;
-//            }
-//        }
+        long bb = board.getBitBoardState().getBitboard(OccupancyBitboard.ALL_PIECES); // copy
+        while (bb != 0) {
+            long lsb = bb & -bb;
+            bb &= bb - 1;
+
+            BoardPiece piece = board.getPieceList()[Long.numberOfTrailingZeros(lsb)];
+            value += getValue(piece);
+        }
+        return (color == Color.WHITE) ? value : -value;
+    }
+
+    private static int getValue(BoardPiece piece) {
+        int value = 0;
+        switch (piece) {
+            case BoardPiece.WHITE_PAWN:
+                value += PAWN_VALUE;
+                break;
+            case BoardPiece.BLACK_PAWN:
+                value -= PAWN_VALUE;
+                break;
+
+            case BoardPiece.WHITE_KNIGHT:
+                value += KNIGHT_VALUE;
+                break;
+            case BoardPiece.BLACK_KNIGHT:
+                value -= KNIGHT_VALUE;
+                break;
+
+            case BoardPiece.WHITE_BISHOP:
+                value += BISHOP_VALUE;
+                break;
+            case BoardPiece.BLACK_BISHOP:
+                value -= BISHOP_VALUE;
+                break;
+
+            case BoardPiece.WHITE_ROOK:
+                value += ROOK_VALUE;
+                break;
+            case BoardPiece.BLACK_ROOK:
+                value -= ROOK_VALUE;
+                break;
+
+            case BoardPiece.WHITE_QUEEN:
+                value += QUEEN_VALUE;
+                break;
+            case BoardPiece.BLACK_QUEEN:
+                value -= QUEEN_VALUE;
+                break;
+
+            case BoardPiece.WHITE_KING:
+                value += KING_VALUE;
+                break;
+            case BoardPiece.BLACK_KING:
+                value -= KING_VALUE;
+                break;
+        }
         return value;
     }
 }
--- src/main/java/dev/lelek/chess/move_generation/MoveGenerator.java
+++ src/main/java/dev/lelek/chess/move_generation/MoveGenerator.java
@@ -1,15 +1,74 @@
 package dev.lelek.chess.move_generation;
 
+import dev.lelek.chess.BoardPosition;
+import dev.lelek.chess.Color;
+import dev.lelek.chess.Move.CastlingMove;
 import dev.lelek.chess.Move.Move;
+import dev.lelek.chess.board.UnmakeMoveInfo;
 import dev.lelek.chess.board.model.Board;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Random;
+import java.util.Stack;
 
 public class MoveGenerator {
 
+    private static final Random random = new Random();
+    static Move bestMove = null;
 
-
-    public static Move generateMove(Board board) {
-        return null;
-
+    public static Move generateMove(Board board, int seconds) {
+        LocalTime time = LocalTime.now();
+        for (int i = 1; ChronoUnit.MILLIS.between(time, LocalTime.now()) < seconds; i++) { // iterative deepening
+            findBestMove(board, new Stack<>(), null, i, i);
+        }
+        return bestMove;
     }
 
+
+    private static int findBestMove(Board board, Stack<UnmakeMoveInfo> unmakeMoveInfos, Move previousMove, int depth, final int startingDepth) {
+        boolean isWhiteToMove = board.isWhiteToMove();
+        Color color = (isWhiteToMove) ? Color.WHITE : Color.BLACK;
+        List<Move> pseudoLegalMoves = PseudoLegalMoveFinder.getPseudoLegalMoves(board, isWhiteToMove);
+        BoardPosition kingPosition = isWhiteToMove ? board.getBlackKingPosition() : board.getWhiteKingPosition();
+
+        if (previousMove instanceof CastlingMove castlingMove) {
+            BoardPosition positionToCheck = new BoardPosition(castlingMove.isKingSideCastling() ? 5 : 3, castlingMove.from().y());
+            if (isPositionTargeted(pseudoLegalMoves, positionToCheck, kingPosition, previousMove.from())) {
+                return Integer.MIN_VALUE / 2;
+            }
+        } else if (isPositionTargeted(pseudoLegalMoves, kingPosition)) {
+            return Integer.MIN_VALUE / 2;
+        }
+
+        if (depth == 0) return BoardEvaluation.evaluate(board, color);
+
+        int best = 0;
+        for (Move move : pseudoLegalMoves) {
+            unmakeMoveInfos.push(new UnmakeMoveInfo(board, move));
+            board.makeMove(move);
+            int foo = findBestMove(board, unmakeMoveInfos, move, depth - 1, startingDepth);
+            foo += random.nextInt(21) - 10;
+            if (foo >= best) {
+                best = foo;
+                if (depth == startingDepth) {
+                    bestMove = move;
+                }
+            }
+            board.unmakeMove(move, unmakeMoveInfos.pop());
+        }
+        return -best;
+    }
+
+    private static boolean isPositionTargeted(List<Move> moves, BoardPosition... positions) {
+        for (Move move : moves) {
+            for (BoardPosition position : positions) {
+                if (move.to().equals(position)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
