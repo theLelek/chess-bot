a3e50e63b6a38af9408818742021f14fe6e4d4fe
Leon Heschl <heschl.leon@gmail.com>
2026-06-16 23:33:37 +0200

    added another mode for the user to play chess via cli

--- src/main/java/dev/lelek/api/Cli.java
+++ src/main/java/dev/lelek/api/Cli.java
@@ -1,5 +1,15 @@
 package dev.lelek.api;
 
+import dev.lelek.chess.BoardPosition;
+import dev.lelek.chess.Move.CastlingMove;
+import dev.lelek.chess.Move.EnPassantMove;
+import dev.lelek.chess.Move.Move;
+import dev.lelek.chess.Move.PromotionMove;
+import dev.lelek.chess.board.BoardPiece;
+import dev.lelek.chess.board.model.Board;
+import dev.lelek.chess.move_generation.GameStatus;
+import dev.lelek.chess.move_generation.MoveGenerator;
+
 import java.util.Scanner;
 
 public class Cli {
@@ -16,19 +26,85 @@ public class Cli {
 
             switch (userInput) {
                 case "uci":
-                    handleUciCommand();
+                    handleUci();
                    break;
-
+                case "play":
+                    handlePlay();
+                    break;
                 default:
                     System.out.println("Invalid command");
             }
         }
     }
 
-    private static void handleUciCommand() {
+    private static void handleUci() {
         System.out.println("id name " + engineName);
         System.out.println("id author " + author);
         System.out.println("uciok");
         Uci.start();
+    }
+
+    private static void handlePlay() {
+        Board board = Board.initializeDefaultBoard();
+        printBoard(board);
+        while (MoveGenerator.getGameStatus(board) == GameStatus.ONGOING) {
+            Move playerMove = initializeMove();
+            board.makeMove(playerMove);
+
+            Move engineMove = MoveGenerator.generateMove(board, (long) 1000); // 1 second for engine move
+            board.makeMove(engineMove);
+            printBoard(board);
+            System.out.println("engine move: " + engineMove);
+            System.out.println("----------------------------");
+        }
+        System.out.println(MoveGenerator.getGameStatus(board));
+    }
+
+    private static Move initializeMove() {
+        Move move;
+        System.out.println("enter your move type");
+        System.out.println("normal move (0), castling move (1), en passant move (2) promotion move (3)");
+        int moveType = Integer.parseInt(scanner.nextLine().trim());
+        Move normalMove = initializeNormalMove();
+
+        switch (moveType) {
+            case 0:
+                move = normalMove;
+                break;
+            case 1:
+                move = (CastlingMove) normalMove;
+                break;
+            case 2:
+                move = (EnPassantMove) normalMove;
+                break;
+            case 3:
+                System.out.println("enter the piece to promote to (e.g. WHITE_KING");
+                move = new PromotionMove(normalMove.from(), normalMove.to(), BoardPiece.valueOf(scanner.nextLine().trim()));
+            default:
+                throw new IllegalArgumentException("invalid move type");
+        }
+        return move;
+    }
+
+    private static Move initializeNormalMove() {
+        System.out.println("enter the move (e.g. e2 e4)");
+        String[] parts = scanner.nextLine().trim().split(" ");
+        return new Move(parts[0], parts[1]);
+    }
+
+    private static void printBoard(Board board) {
+        for (int i = 0; i < Board.SIZE; i++) {
+            for (int j = 0; j < Board.SIZE; j++) {
+                BoardPosition position = new BoardPosition(j, i);
+                BoardPiece piece = board.getPieceList()[position.getBitBoardSquare()];
+                if (piece != null) {
+                    System.out.print(piece.getFen() + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
