a15deedd549a111fcdac22f988391fab9faa8f05
Leon Heschl <heschl.leon@gmail.com>
2026-06-17 13:17:39 +0200

    added todo

--- src/main/java/dev/lelek/chess/move_generation/PseudoLegalMoveFinder.java
+++ src/main/java/dev/lelek/chess/move_generation/PseudoLegalMoveFinder.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PseudoLegalMoveFinder {
-    public static List<Move> getPseudoLegalMoves(Board board, boolean isWhiteToMove) {
+    public static List<Move> getPseudoLegalMoves(Board board, boolean isWhiteToMove) { // todo remove boolena paramether
         List<Move> legalMoves = new ArrayList<>();
         BitBoardState bitBoardState = board.getPosition();
         for (int i = 0; i < BoardPiece.values().length; i++) {
