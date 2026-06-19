7df5a7dab141c343f8a4621e25b630982fb1747e
The.Only.Sensenmann <jannik.sensenberger@gmail.com>
2026-06-19 12:31:50 +0200

    set up main

--- src/main/java/dev/lelek/Main.java
+++ src/main/java/dev/lelek/Main.java
@@ -1,7 +1,18 @@
 package dev.lelek;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@CrossOrigin("*")
 public class Main {
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
         /*
         Bot bot = new Bot(board)
         Board board = new Board();
@@ -13,5 +24,11 @@ public class Main {
         }
 
          */
+    }
+
+    @PostMapping("/chess")
+    public String chess(@RequestBody String message) throws ClassNotFoundException {
+        //handle message
+        return null;
     }
 }
