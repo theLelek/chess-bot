85a1cee47d352be9e898dc7eb66a514c039ab4de
Leon Heschl <heschl.leon@gmail.com>
2026-06-25 10:44:59 +0200

    fixed merge conflicts

--- src/main/java/dev/lelek/Main.java
+++ src/main/java/dev/lelek/Main.java
@@ -1,9 +1,34 @@
 package dev.lelek;
 
-import dev.lelek.api.Cli;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@SpringBootApplication
+@CrossOrigin("*")
 public class Main {
     public static void main(String[] args) {
-        Cli.start();
+        SpringApplication.run(Main.class, args);
+        /*
+        Bot bot = new Bot(board)
+        Board board = new Board();
+
+        while() {
+            board.update(api.getUserMove)
+            Move move = Bot.getMove()
+            print(move)
+        }
+
+         */
+    }
+
+    @PostMapping("/chess")
+    public String chess(@RequestBody String message) throws ClassNotFoundException {
+        //handle message
+        return null;
     }
 }
