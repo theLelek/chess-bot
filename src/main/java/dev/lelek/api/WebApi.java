aa5b75a7b5d172a76db551578015b83ba8e97618
leon heschl <heschl.leon@gmail.com>
2026-07-05 19:55:38 +0200

    added integration tests

--- src/main/java/dev/lelek/api/WebApi.java
+++ src/main/java/dev/lelek/api/WebApi.java
@@ -11,15 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-
 @RestController
 @SpringBootApplication
 @CrossOrigin("*")
 class WebApi {
 
     private final static Uci uci = new Uci();
-    private final ObjectMapper mapper = new ObjectMapper();
 
     static void start() {
         SpringApplication.run(WebApi.class);
@@ -27,11 +24,11 @@ class WebApi {
 
     @PostMapping("/chess")
     String chess(@RequestBody String message) throws JsonProcessingException {
-        System.out.println(message);
+        ObjectMapper mapper = new ObjectMapper();
         String response = null;
         switch (message) {
             case "legal-moves" -> response = mapper.writeValueAsString(LegalMoveFinder.getLegalMoves(uci.getBoard()));
-            case "game-status" -> GameStatus.getGameStatus(uci.getBoard());
+            case "game-status" -> response = GameStatus.getGameStatus(uci.getBoard()).toString();
             default -> response = uci.handleCommand(message);
         }
         return response;
