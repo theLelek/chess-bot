66bc5286ad207c8fea732ab0dcf0fcfd0eec160a
leon heschl <heschl.leon@gmail.com>
2026-07-19 21:48:50 +0200

    added transposition tables

---
+++ src/main/java/dev/lelek/chess/search/TranspositionTable.java
@@ -0,0 +1,35 @@
+package dev.lelek.chess.search;
+
+public class TranspositionTable {
+
+    public static final int SIZE = 65536;
+    private static final TranspositionTable instance = new TranspositionTable();
+
+    private final TranspositionTableEntry[] transpositionTable = new TranspositionTableEntry[SIZE];
+
+    static {
+        if (! (SIZE > 0 && Long.bitCount(SIZE) == 1)) {
+            throw new ExceptionInInitializerError("SIZE must be a power of two: " + SIZE);
+        }
+    }
+
+    private TranspositionTable() {
+    }
+
+    public static TranspositionTable getInstance() {
+        return instance;
+    }
+
+    public TranspositionTableEntry getEntry(long zobristHash) {
+        TranspositionTableEntry entry = transpositionTable[getIndex(zobristHash)];
+        return (entry != null && entry.zobristHash() == zobristHash) ? entry : null;
+    }
+
+    public void setEntry(long zobristHash, TranspositionTableEntry entry) {
+        transpositionTable[getIndex(zobristHash)] = entry;
+    }
+
+    private static int getIndex(long zobristHash) {
+        return (int) (zobristHash & (SIZE - 1));
+    }
+}
---
+++ src/main/java/dev/lelek/chess/search/TranspositionTableEntry.java
@@ -0,0 +1,7 @@
+package dev.lelek.chess.search;
+
+import dev.lelek.chess.Move.Move;
+
+public record TranspositionTableEntry(long zobristHash, Move move, int searchedDepth) { // todo not all of zobristHash needs to be stored
+
+}
